24124b26c52ba5b2f92ac9f7521c10a4377995c5
Florian SIMUNEK <florian.simunek@gmail.com>
2022-08-03 18:51:05 +0200

    :sparkles: Done todos switch in the done list

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // todo list display
 const list = document.querySelector("#list");
+// done todo list display
+const doneList = document.querySelector("#done-list");
 // todo input
 const todo = document.querySelector("#todo");
 // todo add button
@@ -15,8 +17,9 @@ function addTodo(todo) {
     alert("Empty Input !");
   } else {
     // create the li element + button "check" and "remove" - add it to the list
-    let li = "<li>" + todo.value + '<input id="done" type="button" value="V"><input id="remove" type="button" value="X"></li>';
-    list.innerHTML += li;
+    let li = document.createElement("li");
+    li.innerHTML = todo.value + '<input id="done" type="button" value="V"><input id="remove" type="button" value="X">';
+    list.appendChild(li);
     // reset todo input value
     todo.value = "";
   }
@@ -35,8 +38,15 @@ function isInputEmpty(input) {
  * @param {HTMLElement} element - todo li item
  */
 function removeTodo(element) {
-  const parent = element.parentElement;
-  parent.remove();
+  element.parentElement.remove();
+}
+
+/** doneTodo
+ * put the todo item in the done list
+ * @param {HTMLElement} element - todo li item
+ */
+function doneTodo(element) {
+  doneList.appendChild(element.parentElement);
 }
 
 // add todo item when add button is clicked
@@ -44,9 +54,13 @@ add.addEventListener("click", () => {
   addTodo(todo);
 });
 
-// remove todo item when "X" button is clicked
 document.addEventListener("click", (e) => {
+  // remove todo item when "X" button is clicked
   if (e.target && e.target.id == "remove") {
     removeTodo(e.target);
+  }
+  // switch todo to done list
+  if (e.target && e.target.id == "done") {
+    doneTodo(e.target);
   }
 });
