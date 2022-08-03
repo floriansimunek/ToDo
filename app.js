73a389712c4a1dcc9e9532c9711a7286812a9e9f
Florian SIMUNEK <florian.simunek@gmail.com>
2022-08-03 18:13:22 +0200

    :sparkles: Verify if input is not empty or null

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+// todo list display
 const list = document.querySelector("#list");
+// todo input
 const todo = document.querySelector("#todo");
+// todo add button
 const add = document.querySelector("#add");
 
+/** addTodo
+ * add todo item to the todolist
+ * @param {HTMLElement} todo - todo input
+ */
 function addTodo(todo) {
-  let li = "<li>" + todo.value + "</li>";
-  list.innerHTML += li;
-  todo.value = "";
+  // check if input is not empty or null
+  if (isInputEmpty(todo)) {
+    alert("Empty Input !");
+  } else {
+    // create the li element + button "check" and "remove" - add it to the list
+    let li = "<li>" + todo.value + '<input id="done" type="button" value="V"><input id="remove" type="button" value="X"></li>';
+    list.innerHTML += li;
+    // reset todo input value
+    todo.value = "";
+  }
 }
 
-add.addEventListener("click", (e) => {
-  e.preventDefault();
+/** isInputEmpty
+ * return true if the user input is empty
+ * @param {string} input.value
+ */
+function isInputEmpty(input) {
+  return !input.value || input.value === "" || input.value === null;
+}
+
+// add todo item when add button is clicked
+add.addEventListener("click", () => {
   addTodo(todo);
 });
